dbeeeeedc6637066edf65e3a3d3cd2e2b37a1c0f
Maria Dimaano <dimaanomariaisabelle@gmail.com>
2023-02-08 11:05:17 +0800

    configured table relationships and added database transactions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
 
 
 class UserController extends Controller
@@ -14,7 +16,7 @@ class UserController extends Controller
 
     public function createUser(Request $request)
     {
-        $requestData = $request->only(['username', 'password', 'prefix', 'first_name', 'middle_name', 'last_name', 'suffix', 'position_designation']);
+        $requestData = $request->only(['username', 'password', 'prefix', 'first_name', 'middle_name', 'last_name', 'suffix', 'position_designation', 'role_id']);
 
         $validator = Validator::make($requestData, [
             'username'   => 'required|string|min:3',
@@ -25,7 +27,7 @@ class UserController extends Controller
             'suffix'    => 'nullable|present|string',
             'middle_name'    => 'nullable|present|string',
             'position_designation'    => 'required|string',
-
+            'role_id' => 'required|integer|exists:roles,id'
 
         ]);
 
@@ -34,15 +36,19 @@ class UserController extends Controller
         }
 
         try {
+            DB::beginTransaction();
+
             $user = new User([
 
                 'username' => $requestData['username'],
-                'password' => Hash::make($requestData['password'])
+                'password' => Hash::make($requestData['password']),
+                'role_id' => $requestData['role_id']
             ]);
 
             if ($user->save()) {
 
                 $profile = new Profile([
+                    'id' => $user->id,
                     'prefix'               => $requestData['prefix'],
                     'first_name'           => $requestData['first_name'],
                     'middle_name'          => $requestData['middle_name'],
@@ -51,12 +57,17 @@ class UserController extends Controller
                     'position_designation' => $requestData['position_designation'],
                 ]);
 
-                return response()->json(['data' => [$user, 'profile' => $profile], 'message' => 'Successfully created a user'], 201);
+                $profile->save();
+                $user->load('profile');
+
+                DB::commit();
+                return response()->json(['data' => $user, 'message' => 'Successfully created a user'], 201);
             }
         } catch (\Exception$e) {
             report($e);
         }
 
+        DB::rollBack();
         return response()->json(['message' => 'Failed to create a user'], 400);
 
     }
@@ -75,6 +86,21 @@ class UserController extends Controller
             report($e);
         }
         return response()->json(['message' => 'No user/s deleted.'], 400);
+
+    }
+
+    public function getUser(Request $request, $id) {
+
+        $users = User::paginate(10);
+        
+        return response()->json(['data' => $users, 'message' => ' Successfully'], 200);
+
+        // $user = User::with([
+        //     'profile',
+        //     'role'
+        // ])->find($id);
+        
+        // return response()->json(['data' => $user, 'message' => ' Successfully'], 200);
 
     }
 
