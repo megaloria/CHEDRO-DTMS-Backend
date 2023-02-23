28bc0e138d6da630ed60e994e0efe4c8a0a20395
Maria Dimaano <dimaanomariaisabelle@gmail.com>
2023-02-23 15:51:53 +0800

    called classes of ched office seeder and nga seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(
-            UserSeeder::class
+            [UserSeeder::class,
+            NGASeeder::class,
+            ChedOfficeSeeder::class]
+
+            
         );
     }
 }
