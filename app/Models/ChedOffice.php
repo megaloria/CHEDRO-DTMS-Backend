0826c1890b1a34ff12df9ff5c8ebb68f18bbf392
VinceBrianLimbo <binslimbo@gmail.com>
2023-03-13 16:29:42 +0800

    adding public function

--- app/Models/ChedOffice.php
+++ app/Models/ChedOffice.php
@@ -15,4 +15,8 @@ class ChedOffice extends Model
         'description',
         'email'
     ];
+
+    public function sender(){
+        return $this->morphOne('App\Models\Sender','receivable');
+    }
 }
