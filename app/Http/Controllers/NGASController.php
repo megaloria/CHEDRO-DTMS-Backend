ad281d3706049745e846ab9449475eca37de4981
VinceBrianLimbo <binslimbo@gmail.com>
2023-03-14 14:24:20 +0800

    removing S

--- app/Http/Controllers/NGASController.php
+++ app/Http/Controllers/NGASController.php
@@ -110,12 +110,12 @@ class NGASController extends Controller
         })
         ->paginate(6);
 
-        return response()->json(['data' => $ngas, 'message' => 'Successfully fetched the Ched Offices.'], 200);
+        return response()->json(['data' => $nga, 'message' => 'Successfully fetched the Ched Offices.'], 200);
     }
 
     public function getAllNGAS()
     {
-        $ngas = Nga::get();
+        $nga = Nga::get();
 
         return response()->json(['data' => $ngas, 'message' => 'Successfully fetched the NGAs.'], 200);
     }
