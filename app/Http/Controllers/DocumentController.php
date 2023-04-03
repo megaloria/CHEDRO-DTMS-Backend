aa7a84f580ce04809a8b0e95f7f9c97e6d7f8881
Maria Dimaano <dimaanomariaisabelle@gmail.com>
2023-04-03 15:29:57 +0800

    fixed search query for ongoing documents

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -430,40 +430,44 @@ class DocumentController extends Controller
         ->with(['attachments', 'sender.receivable', 'assign.assignedUser.profile', 'logs.user.profile'])
         ->paginate(5);
 
-        $ongoing = Document::whereHas('assign', function ($query) {
-            $query ->whereNotNull('assigned_id');
-        })->whereHas('logs', function ($query) {
-            $query ->whereNotNull('to_id');
-        })->when($searchQuery, function ($query, $searchQuery) {
-            $query->whereHas('documentType', function ($query) use ($searchQuery) {
-                $query->where('description', 'like', "%$searchQuery%");
-            })
-            ->orWhereHas('sender', function ($query) use ($searchQuery) {
-                $query->whereHasMorph('receivable', [ChedOffice::class, Nga::class], function ($query) use ($searchQuery) {
-                    $query->where('description', 'like', "%$searchQuery%");
-                });
-            })->orWhereHas('sender', function ($query) use ($searchQuery) {
-                $query->whereHasMorph('receivable', [Hei::class], function ($query) use ($searchQuery) {
-                    $query->where('name', 'like', "%$searchQuery%");
-                });
-            })
-            ->orWhereHas('category', function ($query) use ($searchQuery) {
-                $query->where('description', 'like', "%$searchQuery%");
-            })
-            ->whereHas('assign', function ($query) {
-                $query ->whereNotNull('assigned_id');
+        $ongoing = Document::when($searchQuery, function ($query, $searchQuery) {
+            $query->whereHas('assign', function ($query) {
+                $query->whereNotNull('assigned_id');
             })
             ->whereHas('logs', function ($query) {
-                $query ->whereNotNull('to_id');
+                $query->whereNotNull('to_id');
             })
-            ->orWhere(function ($query) use ($searchQuery) {
-                $month = date('m', strtotime($searchQuery));
-                $query->whereYear('date_received', $searchQuery)
-                    ->orWhereMonth('date_received', $month)
-                    ->orWhereDay('date_received', $searchQuery);
+            ->where(function ($query) use ($searchQuery) {
+                $query->whereHas('documentType', function ($query) use ($searchQuery) {
+                    $query->where('description', 'like', "%$searchQuery%");
+                })
+                ->orWhereHas('sender', function ($query) use ($searchQuery) {
+                    $query->whereHasMorph('receivable', [ChedOffice::class, Nga::class], function ($query) use ($searchQuery) {
+                        $query->where('description', 'like', "%$searchQuery%");
+                    });
+                })
+                ->orWhereHas('sender', function ($query) use ($searchQuery) {
+                    $query->whereHasMorph('receivable', [Hei::class], function ($query) use ($searchQuery) {
+                        $query->where('name', 'like', "%$searchQuery%");
+                    });
+                })
+                ->orWhereHas('category', function ($query) use ($searchQuery) {
+                    $query->where('description', 'like', "%$searchQuery%");
+                })
+                ->orWhere(function ($query) use ($searchQuery) {
+                    $month = date('m', strtotime($searchQuery));
+                    $query->whereYear('date_received', $searchQuery)
+                ->orWhereMonth('date_received', $month)
+                ->orWhereDay('date_received', $searchQuery);
+                });
             });
-        })->with(['attachments', 'sender.receivable', 'assign.assignedUser.profile', 'logs.user.profile'])
-         ->paginate(5);
+            })->whereHas('assign', function ($query) {
+                $query->whereNotNull('assigned_id');
+            })->whereHas('logs', function ($query) {
+                $query->whereNotNull('to_id');
+            })->with(['attachments', 'sender.receivable', 'assign.assignedUser.profile', 'logs.user.profile'])
+            ->paginate(5);
+
         
         $documentType = DocumentType::get();
         $category = Category::get();
